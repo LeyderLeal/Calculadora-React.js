3747b70e5cc3bee7d9fa96c1227e71053f1f3562
Leyder Leal <leyder.leal2005@gmail.com>
2023-12-26 17:57:57 -0500

    Componente  Boton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/calculadora.png';
+import Boton from './componentes/Boton';
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
         />
       </div>
       <div className='contenedor-calculadora'>
-        <div className='fila'></div>
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>+</Boton>
+        </div>
         <div className='fila'></div>
         <div className='fila'></div>
         <div className='fila'></div>
